90d661c074dd1d5500d8a02f9dab01e344fb15c5
Karina1996 <69156101+Karina1996@users.noreply.github.com>
2022-06-19 04:10:20 +0300

    Update Worker(2-3).sql

--- Worker(2-3).sql
+++ Worker(2-3).sql
@@ -70,39 +70,95 @@ insert into Vacation (ID_Employee, DateBegin, DateEnd)
 -- drop table mysql.Vacation;
 -- delete from mysql.Vacation;
 
-ИСПРАВЛЕНО: [тест](https://app.codingrooms.com/w/O40Tryq9N4IM)
+ИСПРАВЛЕНО: 
 
--- SELECT * FROM mysql.Vacation;
 
-WITH my_subquery_new_dates as
-	( SELECT new_date, SUM(Inc) as sum_Inc
-        FROM (SELECT DateBegin as new_date, 0 as Inc FROM mysql.Vacation 
-              UNION 
-              SELECT DateEnd as new_date, 1 as Inc FROM mysql.Vacation 
-        ) as new_tab_1
-        GROUP BY new_date
-    ),
-    new_tab_2 as
-    ( SELECT V1.DateEnd as DE, V2.DateBegin as DB FROM mysql.Vacation V1 JOIN mysql.Vacation V2 ON V1.ID_Employee != V2.ID_Employee
-        WHERE V1.DateEnd BETWEEN V2.DateBegin AND V2.DateEnd
+-- -- (создание, добавление, выборка, удаление по схеме: mysql.Vacation - БД.Таблица)
+-- -- чтобы выполнить запросы, нажмите FORK в правом верхнем углу
+
+-- -- Отпуска сотрудников
+create table if not exists mysql.Vacation (
+    ID_Employee int not null references Employee(ID), -- связь с родительской таблицей Employee
+    DateBegin date not null,
+    DateEnd date not null
+    );
+
+insert into mysql.Vacation (ID_Employee, DateBegin, DateEnd)
+   values (1, '2022-01-11', '2022-01-25'),
+            (2, '2022-01-11', '2022-01-25'), -- ситуация, когда несколько сотрудников отдыхают в один календарный период 
+            (1, '2022-02-05', '2022-02-19'),
+            (2, '2022-02-10', '2022-02-24'), -- ситуация, когда один из сотрудников ещё в отпуске -> отправился другой
+                (3, '2022-02-10', '2022-02-24'); -- ситуация, когда один из сотрудников ещё в отпуске -> отправился другой
+    
+-- -- drop table mysql.Vacation; (удалить таблицу)
+-- delete from mysql.Vacation; -- очистить данные 
+
+SELECT * FROM mysql.Vacation ORDER BY DateBegin, DateEnd; -- исходные данные
+
+-- - РЕШЕНИЕ 1: (не корректный вывод!)
+
+-- WITH -- обобщенное табличное выражение
+-- -- подзапрос №1 (1)
+-- tab_new_dates as
+--         ( SELECT new_dates, Inc
+--             FROM -- подзапрос №2
+--                 (SELECT DateBegin as new_dates, 0 as Inc FROM mysql.Vacation 
+--                 UNION
+--                 SELECT DateEnd as new_dates, 1 as Inc FROM mysql.Vacation
+--                 ) as tab_union
+--         ),
+-- -- подзапрос №3 (2)
+-- tab_crossing as 
+--         ( SELECT V2.DateBegin as DE, V2.DateEnd as DB, count(V1.ID_Employee) over (partition by V1.ID_Employee) as new_Inc
+--             FROM mysql.Vacation V1 JOIN mysql.Vacation V2 ON V1.ID_Employee != V2.ID_Employee
+--             WHERE V1.DateEnd BETWEEN V2.DateBegin AND V2.DateEnd
+--         ),
+-- -- подзапрос №4 (3) 
+--tab_results as 
+--         (SELECT LAG(new_dates, 1) OVER (ORDER BY new_dates) as DateBegin, 
+--                 new_dates as DateEnd, 
+--                 (CASE ??????????? 
+--                     WHEN new_dates = (SELECT DB FROM tab_crossing WHERE DB = new_dates LIMIT 1) 
+                            -- THEN Inc + (SELECT new_Inc FROM tab_crossing WHERE DB = new_dates LIMIT 1)-1
+--                     WHEN new_dates = (SELECT DE FROM tab_crossing WHERE DE = new_dates LIMIT 1) 
+                            -- THEN Inc + (SELECT new_Inc FROM tab_crossing WHERE DE = new_dates LIMIT 1)-1
+--                     ELSE Inc
+--                 END) as Count
+--         FROM tab_new_dates
+--         )
+
+-- -- ВЫВОД ДАННЫХ В ВИРТУАЛЬНЫХ ТАБЛИЦАХ ВЫПОЛНИТЬ ОДНОВРЕМЕННО НЕ ПОЛУЧИТСЯ!
+-- -- 1. Объединила даты начала и окончания периода в один столбец new_data (с помощью union), установила счётчик, объединила в группы, чтобы посчитать кол-во в группах (каждая группа уникальна).
+-- -- здесь new_dates (уникальные записи), которые нужно сравнить с диапозоном дат пересечений!
+-- -- SELECT new_dates FROM tab_new_dates -- склеили строки значений DateBegin со значениями DateEnd
+
+-- -- 2. Нашла возможные пересечения дат для разных работников (с помощью self join)
+-- SELECT * FROM tab_crossing; -- нашли пересечения дат отпусков для всех дат, которые имеются в исходной таблице
+
+-- -- 3.  В основном подзапросе (tab_results) с помощью оконной функции (функции смещения LAG) взяла предыдущее значение за begin, оставшееся - end, 
+-- -- так как нам нужно взять дату окончания отпуска за дату начала отпуска следующей записи. Оператором case (if) сделала условие, когда выводим 
+-- -- исходный count , также новый инкремент , если пересечение есть (причём нужные значения отбирались в разных столбцах, поэтому пришлось два when прописывать для разных столбцов...
+-- -- SELECT * FROM tab_results; -- выполнили поздапросы в результирующем подзапросе, который вернул, также, таблицу
+
+-- РЕШЕНИЕ 2: (ОК)
+-- посчитать с помощью оконной функции получилось, УРА!
+WITH
+  tab_new_dates AS (
+    SELECT new_dates, SUM(Inc) AS Inc
+      FROM (
+        SELECT '2022-01-01' AS new_dates, 0 AS `inc`
+        UNION ALL
+          SELECT '2022-12-31', 0
+        UNION ALL
+          SELECT DateBegin as new_dates, +1
+            FROM mysql.Vacation
+        UNION ALL
+          SELECT DateEnd as new_dates, -1
+            FROM mysql.Vacation
+      ) AS tab_union
+      GROUP BY new_dates
     )
-
-SELECT LAG(new_date, 1) OVER (ORDER BY new_date) as DateBegin, 
-            new_date as DateEnd, 
-            (CASE 
-                WHEN new_date IN(SELECT DE FROM new_tab_2) THEN sum_Inc+1
-                WHEN new_date IN(SELECT DB FROM new_tab_2 ) THEN sum_Inc+1
-                ELSE sum_Inc
-            END) as Count
-FROM my_subquery_new_dates;
-
-/*На первом шаге (в первом подзапросе) составила даты всех возможных интервалов, потому что дата окончания отпуска какого-то сотруднгика будет датой начала нового интервала. То есть, нужно чтобы даты начала интервала и даты окончания интервала находились в одной колонке. Без оператора UNION этого не сделать не получилось. Немного помогло: https://www.sqlshack.com/sql-lag-function-overview-and-examples.
-Намучилась с подсчетом кол-ва..
-
-С помощью оконной функции вывела предыдущее значение столбца по порядку сортировки new_date - LAG(new_date, 1) OVER (ORDER BY new_date)
-
-Оконная функция - функция, которая работает с выделенным набором строк (окном, партицией) и выполняет вычисление для этого набора строк в отдельном столбце. 
-
-Главное отличие оконных функций от функций агрегации с группировкой? 
-При использовании агрегирующих функций предложение GROUP BY сокращает количество строк в запросе с помощью их группировки.
-При использовании оконных функций количество строк в запросе не уменьшается по сравнении с исходной таблицей.*/
+SELECT LAG(new_dates, 1) OVER (ORDER BY new_dates) AS DateBegin,
+       new_dates AS DateEnd,
+       (SUM(Inc) OVER (ORDER BY new_dates)) - Inc AS Count
+  FROM tab_new_dates
